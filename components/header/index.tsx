1d3c63fb713856c66452c8e213afe609dcf9d28f
cy-bug <shashwat5590@gmail.com>
2023-09-14 11:27:51 +0530

    minor color and layout changes.

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Github } from "lucide-react";
 function Header() {
   return (
-    <header className="container flex flex-row items-center justify-between">
-      <h1 className="text-2xl font-semibold">Shortly</h1>
+    <header className="container flex flex-row items-center justify-between px-6">
+      <h1 className="text-2xl font-semibold text-stone-200">Shortly</h1>
       <a
         href="https://github.com/phantomknight287/url-shortner"
         target="_blank"
